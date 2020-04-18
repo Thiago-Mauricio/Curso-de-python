4bff4af4c25dc3d3876d1f0a528e7cdaaaefd517
THIAGO MAURICIO LEITE <thiagomprog@gmail.com>
2020-04-18 16:36:24 -0300

    i

--- Curso em Video/Aula08 Utilizando Módulos/Ex_021 Tocando MP3.py
+++ Curso em Video/Aula08 Utilizando Módulos/Ex_021 Tocando MP3.py
@@ -1,10 +1,10 @@
-from typing import List
-
 import pygame
+
 #iniciando o mixer do pygame
 pygame.mixer.init()
 #iniciando o pygame
 pygame.init()
-pygame.mixer.music.load('ex21.mp3')
+
+pygame.mixer.music.load('Ex_21.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()
