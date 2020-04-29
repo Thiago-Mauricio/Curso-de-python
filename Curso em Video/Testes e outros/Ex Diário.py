2762b272733faea28df7e8edf088fe4190f66ec7
THIAGO MAURICIO LEITE <thiagomprog@gmail.com>
2020-04-29 17:53:10 -0300

    Update Ex Diário.py

--- Curso em Video/Testes e outros/Ex Diário.py
+++ Curso em Video/Testes e outros/Ex Diário.py
@@ -1,36 +1,23 @@
 from random import randint
 print(randint(10, 121))
 
-def aluguel(dias, km):
-    total = (dias * 60) + (km * 0.15)
-    print(f'O valor total a ser pago é de R$: {total:.2f}')
-
-
-print('=' * 40)
-print('{:^40}'.format('Aluguel de Carros.'))
-print('=' * 40)
 while True:
-    while True:   
+    while True:    
         try:
-            dias = int(input('Por quantos dias o carro foi alugado? '))
+            n1 = int(input('Digite um valor: '))
+            n2 = int(input('Digite outro valor: '))
             break
         except:
-            print('Erro: digite apenas números inteiros! ')
-    print('-' * 40)
-    while True:
-        try:
-            km = float(input('Quantos Kms foram percorridos? '))
-            break
-        except:
-            print('Erro: Valor digitado inválido, digite apenas números e pontos para casas decimais')
-    print('-' * 40)        
-    aluguel(dias, km)
-    print('-' * 40)
+            print('Erro: Ambos os valores precisam ser números inteiros:')
+    if n1 > n2:
+        print(f'{n1} é maior que {n2} !')
+    elif n1 == n2:
+        print (f'{n1} e {n2} são iguais!')
+    else:
+        print(f'{n2} é maior que {n1} !')
     stop = ''
     while stop != 'N' and stop != 'S':
-        stop = input('Deseja saber outro valor de aluguel? [S/N] ').strip().upper()
+        stop = input('Deseja continuar? [S/N] ').strip().upper()
     if stop == 'N':
         break
-print('=' * 40)
-print('{:^40}'.format('Programa Finalizado!'))
-print('=' * 40)
+print('FIM !')
